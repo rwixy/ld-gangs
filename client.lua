d9442200ff380a9138f2fc561b64530fb1ed0d74
Ihsan <122683406+rwixy@users.noreply.github.com>
2025-05-18 11:29:56 +0530

    Update client.lua

--- client.lua
+++ client.lua
@@ -12,15 +12,7 @@
 
 local PlayerData = {}
 
-ESX = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
 Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-        Citizen.Wait(0)
-    end
-    
     while ESX.GetPlayerData().job == nil do
         Citizen.Wait(10)
     end
